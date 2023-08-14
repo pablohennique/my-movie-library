8be57d6aa9f68bf7308c68b29706f8e6a7647cb8
Pablo Hennique <phennique@gmail.com>
2023-08-14 11:03:20 -0400

    added zoom funcitonality to the movie image using event.target.classList.toggle

--- src/components/movie-poster.js
+++ src/components/movie-poster.js
@@ -4,6 +4,14 @@ import styles from './movie-poster.module.css'
 
 export function MoviePoster(posterPathObject) {
   return (
-    <img src={posterPathObject.posterPath} className={styles.poster}></img>
+    <img
+      src={posterPathObject.posterPath}
+      className={styles.poster}
+      onClick={(event) => {
+        // console.log(event.target);
+        event.target.classList.toggle('zoom')
+      }}
+    >
+    </img>
   )
 }
