2be40e310015b22ea9c8f0e258c549734d075a0f
Pablo Hennique <phennique@gmail.com>
2023-08-22 16:19:18 -0400

    added forgivingHovering with useState and UseEffect

--- src/components/burger-menu.js
+++ src/components/burger-menu.js
@@ -1,26 +1,58 @@
 'use client'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { NavMenu } from "./nav-menu"
 import { ThemeSwitch } from "./theme-switch"
 import styles from './burger-menu.module.css'
+const delay = (ms) => new Promise((r) => setTimeout(r, ms))
 
 export function BurgerMenu() {
   const burgerMenuIcon = <FontAwesomeIcon icon={faBars} size='2xl' style={{color: "#f8a61a", }} />
   const [isMenuOpen, setIsMenuOpen] = useState(false)
-  const menuShown = () => {
+  const [isMouseOn, setIsMouseOn] = useState(false)
+
+  const handleMouseEnteringBar = () => {
+    setIsMouseOn(true)
+  }
+
+  const handleMouseLeavingBar = () => {
+    setIsMouseOn(false)
+  }
+
+  const handleMouseEnteringBurger = () => {
     setIsMenuOpen(true)
     document.body.classList.add('swipeDown')
   }
-  const menuHidden = () => {
-    setIsMenuOpen(false)
-    document.body.classList.remove('swipeDown')
-  }
+
+  useEffect(() => {
+    let isCurrent = true
+
+    const forgivingHover = async () => {
+      await delay(500)
+      if(!isMouseOn && isCurrent) {
+        setIsMenuOpen(false)
+        document.body.classList.remove('swipeDown')
+      }
+    }
+    forgivingHover()
+
+    return() => {
+      isCurrent = false
+    }
+  }, [isMouseOn])
+
+  console.log(`is mouse on:${isMouseOn}`);
+  console.log(`is menu open:${isMenuOpen}`);
 
   return(
-    <div className={styles.navBarComplete} onMouseLeave={menuHidden}>
-      <div onMouseEnter={menuShown} >
+    <div className={styles.navBarComplete}
+      onMouseLeave={handleMouseLeavingBar}
+      onMouseEnter={handleMouseEnteringBar}
+    >
+      <div
+        onMouseEnter={handleMouseEnteringBurger}
+      >
         {burgerMenuIcon}
       </div>
       {isMenuOpen && <NavMenu />}
